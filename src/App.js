57e489dc4cc768fda089a199dd5129d35ef9e3da
techamit95ch <amit98ch@gmail.com>
2021-10-02 21:56:22 +0530

    create card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {Card, CardHeader, CardContent, Typography} from '@material-ui/core'
+import {Doughnut} from "react-chartjs-2"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Card>
+    <CardHeader title="Income"/>
+    <CardContent>
+      <Typography variant="">$50</Typography>
+      <Doughnut data="data"/>
+    </CardContent>
+      
+    </Card>
+  )
 }
 
-export default App;
+export default App
